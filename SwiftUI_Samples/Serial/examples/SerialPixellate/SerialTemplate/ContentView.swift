fb03b34a45ca502ff2ab4f92bf672c845a1e0085
Peter Rogers <p.rogers@gold.ac.uk>
2025-11-11 01:02:55 +0000

    changed pixel code

--- SwiftUI_Samples/Serial/examples/SerialPixellate/SerialTemplate/ContentView.swift
+++ SwiftUI_Samples/Serial/examples/SerialPixellate/SerialTemplate/ContentView.swift
@@ -12,45 +12,29 @@ struct ContentView: View {
     @State private var serialModel = SerialModel()
     
     var body: some View {
-        ZStack {
+        ZStack{
             Rectangle()
                 .fill(.white)
-//            Slider(value: $serialModel.pixel, in: 0...1)
-//                .padding(.horizontal, 200)
-//                Image("stainWindow")
-//                    .resizable()
-//                    .scaledToFit()
-//                    .padding(25)
-//                    .background(.white)
-//                    .drawingGroup()
-//                    .aspectRatio(contentMode: .fit)
-//                    .distortionEffect(
-//                        
-//                        ShaderLibrary.pixellate(
-//                            .float(serialModel.pixel * 100)
-//                        ),
-//                        maxSampleOffset: .zero
-//                    )
-            Image("stainWindow")
-                .resizable()
-                .scaledToFit()
-                .padding(25)
-                .background(.white)
-                .drawingGroup()
-                .distortionEffect(ShaderLibrary.water(
-                    .float2(serialModel.pixel * 400, serialModel.pixel * 400),
-                    .float(serialModel.pixel * 10),
-                    .float(100),
-                    .float(10),
-                    .float(2)
-                    
-                ),maxSampleOffset: .zero
-                )
+            VStack {
+                Image("stainWindow")
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .distortionEffect(
+                        ShaderLibrary.pixellate(
+                            .float(serialModel.pixel * 100)
+                        ),
+                        maxSampleOffset:.zero)
+                
+                Slider(value: $serialModel.pixel, in: 0...1)
+                    .padding(.horizontal, 200)
+                    .padding(.bottom, 100)
+                
+            }
         }.edgesIgnoringSafeArea(.all)
-        .background(.white)
-        .onAppear {
-            serialModel.startSerial()
-        }
+            .background(.white)
+            .onAppear {
+                serialModel.startSerial()
+            }
     }
 }
 
